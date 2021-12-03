c48fa2b6f4fa3a36706d63b08d495afee22f1961
Anatoly <kaellakhim@gmail.com>
2021-12-04 00:19:21 +0200

    1 rfncwtyf

---
+++ Assets/Scripts/CutSceneCharSCR.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutSceneCharSCR : Unit
+{
+    [SerializeField]
+    private float speedwalk = 2.0F;
+    new public Rigidbody2D rigidbody;
+    private Animator animator;
+    private SpriteRenderer sprite;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private CharStateC State
+    {
+        get { return (CharStateC)animator.GetInteger("State"); }
+        set { animator.SetInteger("State", (int)value); }
+    }
+
+    private void Update()
+    {
+        State = CharStateC.Idle;
+
+        if (Input.GetButton("Horizontal")) Run();
+    }
+    private void Run()
+    {
+        Vector3 direction = transform.right * Input.GetAxis("Horizontal");
+        if (direction.x > 0)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speedwalk * Time.deltaTime);
+            //sprite.flipX = direction.x < 0;
+            State = CharStateC.Run;
+        }
+        else sprite.flipX = direction.x < 0;
+    }
+  
+}
+
+public enum CharStateC
+{
+    Idle,
+    Run
+}
+
--- Assets/Scripts/Dialog.cs
+++ Assets/Scripts/Dialog.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class Dialog : MonoBehaviour
 {
     public GameObject panelDialog;
+    public GameObject thoseWhoSay;
     private void Start()
     {
+       // panelDialog.transform.position = thoseWhoSay.transform.position;
         panelDialog.SetActive(false);
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("Вошел в диалог");
         if (collision.tag == "Player")
         {
             panelDialog.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Вышел из диалога");
+        if (collision.tag == "Player")
+        {
+            panelDialog.SetActive(false);
         }
     }
 }
